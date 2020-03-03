b26b746897fb6b8b50d91afaa2f9d64342f6cfbd
WaiEnu <strayhituj@gmail.com>
2020-03-03 19:35:56 +0900

    20.03.03 ビルド

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: "./",
-    assetsDir: "",
-    outputDir: "docs"
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dsui/'
+    : '/',
+  outputDir: 'docs',
+}
