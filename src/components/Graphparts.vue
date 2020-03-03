fa090a26b51869cc1f8521fc49eb48df76b43a8b
WaiEnu <strayhituj@gmail.com>
2020-03-03 23:21:13 +0900

    20.03.03 ビルド

--- src/components/Graphparts.vue
+++ src/components/Graphparts.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="graphparts">
-  <div id="graph_origin">
-    ## graph_o ##
-  </div>
-  <div id="graph_mutate">
-    ## graph_m ##
-  </div>
+    <div class="contents" v-if="activePageName == 'origin'">
+      <div id="graph_origin">
+        ## graph_o ##
+      </div>
+      <div id="graph_mutate">
+        ## graph_m ##
+      </div>
+    </div>
   </div>
 </template>
 
