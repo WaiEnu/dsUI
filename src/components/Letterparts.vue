f94f95e9f621702cdbe1b390820efa6e91896fd6
WaiEnu <strayhituj@gmail.com>
2020-03-06 20:20:56 +0900

    20.03.06 Letters修正

--- src/components/Letterparts.vue
+++ src/components/Letterparts.vue
@@ -4,7 +4,7 @@
       <div>
         <ul class="list">
           <transition-group name="flip">
-            <template v-for="item in letter(align)">
+            <template v-for="item in letterX">
               <li :key="item.id" class="item" :id="item.id">
                 <div class="item__text">{{ item.text }}</div>
               </li>
@@ -13,6 +13,17 @@
         </ul>
         <div><button>ロボット</button>
         <button>伝票</button></div>
+      </div>
+      <div class="vs-modal" style="display: block;">
+        <ul class="list">
+          <transition-group name="flip">
+            <template v-for="item in letter">
+              <li :key="item.id" class="item" :id="item.id">
+                <div class="item__text">{{ item.text }}</div>
+              </li>
+            </template>
+          </transition-group>
+        </ul>
       </div>
     </div>
   </div>
@@ -23,46 +34,42 @@ export default {
   name: 'Letterparts',
   data() {
     return {
-      dna: ['A','G','C','T'],
+      dna: 'TACACCAGCATCACT',
+      acd: ['A','G','T','C'],
       stt: 'TAC',
       stps: ['ATT','ATC','ACT'],
-      list: 3,
-      GC: 0.50,
     }
   },
   computed: {
-    align(){
-      return this.$data.stt + makeDNA(this.$data.list*3,this.$data.GC) + this.$data.stps[Math.floor(this.$data.stps.length * Math.random())]
-    } 
+    letter(){
+      const letters=this.$data.dna.split('');
+      return this.newSeq(letters)
+    },
+    letterX(){
+      const letters=this.$data.dna.split('');
+      const index=Math.floor(Math.random()*letters.length);
+      const flg=Math.floor(Math.random()*5);
+      const lettersX=this.rndDNA(letters,index,flg);
+      return this.newSeq(lettersX)
+    }
   },
   methods: {
-    letter(str){
-      const letters=str.split('');
+    rndDNA(arr,index,flg) {
+      const letters=arr;
+      if(flg==5){
+        letters.splice(index, 1); 
+      }else{
+        letters.splice(index, 0, this.$data.acd[flg]);
+      }
+      return letters;
+    },
+    newSeq(letters) {
       let newseq=[]
       for (var i = 0; i < letters.length; i++) {
-        newseq.push( {'id':'id_'+i,'text':letters[i]});
+        newseq.push( {'id':'id_'+i+1,'text':letters[i]});
       }
       return newseq
-    }
+    },
   }
-}
-function makeDNA(size,GC) {
-	var newseq="";
-	var letter="";
-	for (var i = 0; i < size; i++) {
-    if (rnd() < GC) {
-      letter=(rnd()<0.5) ? "C" : "G";
-    } else {
-      letter=(rnd()<0.5) ? "T" : "A";
-    }
-    newseq+=letter;
-	}
-	return newseq;
-}
-rnd.today=new Date();
-rnd.seed=rnd.today.getTime();
-function rnd() {
-  rnd.seed = (rnd.seed*9301+49297) % 233280;
-  return rnd.seed/(233280.0);
 }
 </script>
