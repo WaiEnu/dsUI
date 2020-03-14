5fb58ca11a212a24cf91e05b328869eddf8f4867
WaiEnu <strayhituj@gmail.com>
2020-03-14 20:22:49 +0900

    20.03.14

--- src/components/Letterparts.vue
+++ src/components/Letterparts.vue
@@ -65,7 +65,7 @@
       <v-col cols="10">
         <ul class="list pl-0">
           <transition-group name="flip">
-            <template v-for="item in codon">
+            <template v-for="item in codonX">
               <li :key="item.id" class="item" :id="item.id">
                 <div class="item__text">{{ item.text }}</div>
                 <div class="item__"></div>
@@ -77,7 +77,7 @@
     </v-row>
     <v-row no-gutters="">
       <v-col cols="4" class="text-center pa-2">
-        <v-btn>ロボット</v-btn>
+        <v-btn @click="onRobotClick()">ロボット</v-btn>
       </v-col>
       <v-col cols="4" class="text-center pa-2">
         <v-btn>伝票</v-btn>
@@ -94,48 +94,34 @@ export default {
   name: 'Letterparts',
   data() {
     return {
-      ali: [2,0,3,0,3,3,0,1,3,0,2,3,0,3,2],
-      alX: [2,0,3,0,3,3,0,1,3,0,2,2,3,0,3,2],
+      rnk: 0,
+      ali: [[2,0,3,0,3,3,0,1,3,0,2,3,0,3,2]],
+      alX: [[2,0,3,0,3,3,0,1,3,0,2,3,3,0,3,2]],
+      tmp: [[2,0,3,0,3,3,0,1,3,0,2,3,3,0,3,2]],
       dna: ['A','G','T','C'],
       rna: ['U','C','A','G'],
-      cdn: [
-        {amino:'F',codon:['000','001']},
-        {amino:'L',codon:['002','003','100','101','102','103']},
-        {amino:'I',codon:['200','201','202']},
-        {amino:'M',codon:['203']},
-        {amino:'V',codon:['300','301','302','303']},
-        {amino:'S',codon:['010','011','012','013','230','231']},
-        {amino:'P',codon:['110','111','112','113']},
-        {amino:'T',codon:['210','211','212','213']},
-        {amino:'A',codon:['310','311','312','313']},
-        {amino:'Y',codon:['020','021']},
-        {amino:'H',codon:['120','121']},
-        {amino:'Q',codon:['122','123']},
-        {amino:'N',codon:['220','221']},
-        {amino:'K',codon:['222','223']},
-        {amino:'D',codon:['320','321']},
-        {amino:'E',codon:['322','323']},
-        {amino:'C',codon:['030','031']},
-        {amino:'W',codon:['033']},
-        {amino:'R',codon:['130','131','132','133','232','233']},
-        {amino:'G',codon:['330','331','332','333']},
-        {amino:'STOP',codon:['022','023','032']}
-      ],
+      cdn:[[['F','F','L','L'],['S','S','S','S'],['Y','Y','x','x'],['C','C','x','W']],[['L','L','L','L'],['P','P','P','P'],['H','H','Q','Q'],['R','R','R','R']],[['I','I','I','M'],['T','T','T','T'],['N','N','K','K'],['S','S','R','R']],[['V','V','V','V'],['A','A','A','A'],['D','D','E','E'],['G','G','G','G']],],
     }
   },
   computed: {
-    letter: function (){
-      return this.newSeq(this.$data.ali)
+    alignX: function (){
+      return this.$data.alX[this.$data.rnk]
     },
     letterX: function (){
-      return this.newSeq(this.$data.alX)
+      return this.newSeq(this.alignX)
     },
     copyX: function (){
-      return this.tranSeq(this.$data.alX)
+      return this.tranSeq(this.alignX)
     },
-    codon: function (){
-      return this.makecodon(this.$data.alX)
-    }
+    codonX: function (){
+      return this.makecodon(this.alignX)
+    },
+    alignI: function (){
+      return this.$data.ali[this.$data.rnk]
+    },
+    codonI: function (){
+      return this.makecodon(this.alignI)
+    },
   },
   methods: {
     newSeq: function (letters) {
@@ -158,22 +144,33 @@ export default {
       const n = 3;
       let codon = [];
       let i = 1;
-      let idx = 3;
-      while(idx < letters.length){
-        let val=[letters[idx-n],letters[idx-n+1],letters[idx-n+2]].join('');
-        codon.push({'id':i,'text':this.transcodon(val)});
-        idx += n;
+      let d = 3;
+      while(d < letters.length){
+        codon.push({'id':i,'text':this.$data.cdn[letters[d-n]][letters[d-n+1]][letters[d-n+2]]});
+        d += n;
         i ++;
       }
       return codon;
     },
-    transcodon(val){
-      let key = this.$data.cdn.filter(e => e.codon == val);
-      return key.amino;
-    },
     onDeleteClick: function (item) {
       const index = item;
-       this.$data.alX.splice(index, 1);
+      this.alignX.splice(index, 1);
+    },
+    onRobotClick: function () {
+      if(JSON.stringify(this.codonX) === JSON.stringify(this.codonI)){
+        alert('sucsess');
+        this.next();
+      }else{
+        alert('failed')
+      }
+      return ;
+    },
+    next: function () {
+      if(this.$data.rnk+1<this.$data.ali.length){
+        this.$data.rnk++
+      }else{
+        this.$router.go({path: this.$router.currentRoute.path, force: true})
+      }
     },
     reset: function () {
       this.$router.go({path: this.$router.currentRoute.path, force: true})
